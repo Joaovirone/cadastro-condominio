c292757eef858d2e2e7e1812b7cb4a248c3948d0
joaovirone <jovmamikl@gmail.com>
2026-01-15 21:26:22 -0300

    INICIANDO REPOSITORIO DO PROJETO

---
+++ src/main/java/com/cadastroCondominio/cadastro_condominio/CadastroCondominioApplication.java
@@ -0,0 +1,13 @@
+package com.cadastroCondominio.cadastro_condominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroCondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroCondominioApplication.class, args);
+	}
+
+}
